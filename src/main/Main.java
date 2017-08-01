7ecb173687b742c5d549d57c5176c3714b1ccc93
jackaroth <huangxjack@gmail.com>
2017-08-01 16:26:52 +1200

    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
 
@@ -32,10 +33,40 @@ public class Main {
 		try {
 		    reader = new BufferedReader(new FileReader(file));
 		    String text = null;
+		    text = reader.readLine();
+		    String[] headerArray = text.split("\"");
+		    String digraphName = headerArray[1];
 		    
 		    while ((text = reader.readLine()) != null && (text != "}") ) {
 		    	
-		    	System.out.println(reader.readLine());
+		    	System.out.println(text);
+		    	
+		    	String[] lineArray=text.split("[");
+		    	
+		    	//read the weight
+		
+		    	String weightString = lineArray[1];      
+		    	weightString = weightString.replaceAll("[^0-9]+", " ");
+		    	String[] weightArray = weightString.trim().split(" ");
+		    	int weight =Integer.parseInt(weightArray[0]);
+		    	System.out.println(weight);
+		    	
+		    	//Read node
+		    	
+		    	if (lineArray[0].contains("->")) {
+		    		String[] nodeArray = lineArray[0].split("->");
+		    		char nodeA = nodeArray[0].trim().charAt(0);
+		    		System.out.println(nodeA);
+		    		char nodeB = nodeArray[1].trim().charAt(1);
+		    		System.out.println(nodeB);
+		    	} else {
+		    		String nodeString = lineArray[0].trim();
+		    		char node = nodeString.charAt(0);
+		    		System.out.println(node);
+		    	}
+		    	
+		    	
+		    	//comment
 		    	
 		    }
 		    
