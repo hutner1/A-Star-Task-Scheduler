1247b4063ca79f8c6e30080b67f144ac314a3623
hbao448 <hbao448@aucklanduni.ac.nz>
2017-08-01 17:40:58 +1200

    updated vertex structure

--- src/main/Vertex.java
+++ src/main/Vertex.java
@@ -1,11 +1,15 @@
 public class Vertex {
 	
+	private String _name;
 	private double _weight;
-	
-	public Vertex (double weight) {
+	public Vertex (String name, double weight) {
+		_name = name;
 		_weight = weight;
 	}
-	
+
+	public String getName() {
+		return _name;
+	}
 	public double getWeight() {
 		return _weight;
 	}
