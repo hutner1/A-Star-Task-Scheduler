a61d0b200e6516ab720ab36270681494b19c4d98
Yao Jian Yap <yyap601@aucklanduni.ac.nz>
2017-08-03 00:22:47 +1200

    Added convenient line to file recorder in Main

--- src/scheduler/Main.java
+++ src/scheduler/Main.java
@@ -1,9 +1,12 @@
 package scheduler;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -216,6 +219,23 @@ public class Main {
 	public static void InputError(String msg){
 		System.out.println(msg);
 		System.exit(0);
+	}
+	
+	
+	/**
+	 * Convenient method to record a single line to a file
+	 * @param file file to write to
+	 * @param line line of words to record to a file
+	 */
+	public static void record(File file,String line){
+		try{
+			PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(file,true)));
+			writer.println(line);
+			writer.close();
+		} catch(IOException e){
+			System.out.println("An I/O Error Occurred when trying to write to " + file.toString());
+			System.exit(0);
+		}
 	}
 
 }
