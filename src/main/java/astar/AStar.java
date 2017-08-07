31eece68e72d9e1068268e0bcb0e80406250ff5c
Jack Huang <xhua451@UoA.auckland.ac.nz>
2017-08-08 11:55:03 +1200

    Initialised astar main

--- src/main/java/astar/AStar.java
+++ src/main/java/astar/AStar.java
@@ -1,5 +1,15 @@
 package astar;
 
-public class AStar {
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
 
+import scheduler.basicmilestone.Vertex;
+
+public class AStar {
+	private DefaultDirectedWeightedGraph<Vertex, DefaultWeightedEdge> _graph;
+	public AStar(DefaultDirectedWeightedGraph<Vertex, DefaultWeightedEdge> graph) {
+		_graph = graph;
+	}
+	
+	
 }
