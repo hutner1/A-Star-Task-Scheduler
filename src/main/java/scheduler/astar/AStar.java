1a85a13427a009aefab6dc9a972ca336a4a534e8
hbao448 <hbao448@aucklanduni.ac.nz>
2017-08-08 17:05:15 +1200

    WIP

--- src/main/java/scheduler/astar/AStar.java
+++ src/main/java/scheduler/astar/AStar.java
@@ -19,6 +19,7 @@ public class AStar {
 	public void execute() {
 
 		List<Solution> initialSolutions = new ArrayList<Solution>();
+		List<Vertex> scheduledProcesses = new ArrayList<Vertex>();
 		List<Vertex> schedulableProcesses = new ArrayList<Vertex>();
 		List<Vertex> nonschedulableProcesses = new ArrayList<Vertex>();
 
@@ -46,10 +47,39 @@ public class AStar {
 		for (Vertex v : schedulableProcesses) {
 			for (int i = 1; i <= _numberOfProcessors; i++) {
 				bestInitialSolution.addProcess(v, i);
+				updateSchedulable(v, scheduledProcesses, schedulableProcesses, nonschedulableProcesses);
 			}
 		}
 		
 		
 	}
 
+	/**
+	 * This method updates the list of schedulable and nonschedulable processes.
+	 * It checks the children processes of the parent input and if all of the parent processes
+	 * of the child have already been scheduled
+	 * @param parent Parent process to be updated
+	 * @param scheduled List of processes already scheduled
+	 * @param schedulable List of processes available to be scheduled
+	 * @param nonschedulable List of processes that have not had their dependencies met
+	 */
+	private void updateSchedulable(Vertex parent, List<Vertex> scheduled, List<Vertex> schedulable, List<Vertex> nonschedulable) {
+		
+		scheduled.add(parent);
+		
+		for (DefaultWeightedEdge outEdge : _graph.outgoingEdgesOf(parent)) {
+			Vertex child = _graph.getEdgeTarget(outEdge);
+			
+			boolean canBeScheduled = true;
+			for (DefaultWeightedEdge inEdge : _graph.incomingEdgesOf(child)) {
+				if (!scheduled.contains(_graph.getEdgeSource(inEdge))) {
+					canBeScheduled = false;
+				}
+			}
+			if (canBeScheduled) {
+				schedulable.add(child);
+				nonschedulable.remove(child);
+			}
+		}
+	}
 }
