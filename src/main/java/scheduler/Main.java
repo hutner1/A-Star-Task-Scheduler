db9e2edac36105e375e6df1a4529f4ed349a6da7
Unknown <h.chuk@auckland.ac.nz>
2017-08-14 22:27:16 +1200

    Changed Main() to used AStar

--- src/main/java/scheduler/Main.java
+++ src/main/java/scheduler/Main.java
@@ -47,7 +47,7 @@ public class Main {
 			List<Vertex> tSort = sorter.generateSort();
 			Schedule sol = ScheduleGenerator.makeSolution(tSort);*/
 			long startTime = System.nanoTime();
-			AStar aStar = new AStarParallelised(dataReader.getGraph(),inputParser.getProcessors(),4, graphVisualizer);
+			AStar aStar = new AStar(dataReader.getGraph(),inputParser.getProcessors(), graphVisualizer);
 			Solution optimalSolution = aStar.execute();
 			outWriter.createScheduleAStar(dataReader.getGraphName(),dataReader.getInfo(),dataReader.getRead(),optimalSolution,dataReader.getMapping());
 			long endTime = System.nanoTime();
