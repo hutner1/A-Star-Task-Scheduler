53f023b3c029aa8d720159eb9fe3e524d1665543
hbao448 <hbao448@aucklanduni.ac.nz>
2017-08-14 22:37:37 +1200

    removed obsolete method

--- src/main/java/io/OutputWriter.java
+++ src/main/java/io/OutputWriter.java
@@ -12,7 +12,6 @@ import java.util.HashMap;
 import scheduler.astar.Solution;
 import scheduler.dfsbranchandbound.SolutionGenerator;
 import scheduler.graphstructures.Vertex;
-import scheduler.topologicalsort.Schedule;
 
 /**
  * Writes optimal solution to a file
@@ -46,41 +45,6 @@ public class OutputWriter {
 			clearFile(_outputFile);
 		}
 	}
-
-
-	/**
-	 * Create a schedule and write it to the output dot file 
-	 * @param digraphName name of the digraph
-	 * @param weightInfos the entries in the input file which will have 2 extra properties appended to it in the end for the output file
-	 * @param verticesAndEdges list that contains all the tasks and edges as ">", in the same order as the input digraph, to have the schedule be in the same order as the input digraph
-	 * @param solution Schedule containing Vertices and NodeInfos
-	 */
-	public void createSchedule(String digraphName, ArrayList<String> weightInfos, ArrayList<String> verticesAndEdges, Schedule solution, HashMap<String, Vertex> vertexMapping){
-
-		//record first line of output file which contains the title
-		record(_outputFile, "digraph \"" + digraphName +"\" {");
-
-		//record the weight info together with the start time and processor, in order according to input file 
-		for(String info : weightInfos){
-			int currentPos = weightInfos.indexOf(info);
-			// edge would be ">"
-			String vertexOrEdge = verticesAndEdges.get(currentPos);
-
-			// record initially recorded edge info directly back to file as no extra info is needed
-			if(vertexOrEdge.equals(">")){
-				record(_outputFile, info);
-
-			} else {
-				// add the start and processor info to the end before closing bracket
-				StringBuilder augmentedInfo = new StringBuilder(info).insert(info.length()-2, solution.getVertexInfo(vertexMapping.get(vertexOrEdge)).outputString());
-				record(_outputFile, augmentedInfo.toString());
-			}
-		}
-
-		//end the output file with closing bracket
-		record(_outputFile, "}");
-	}
-	
 
 	public void createScheduleAStar(String digraphName, ArrayList<String> weightInfos, ArrayList<String> verticesAndEdges, Solution solution, HashMap<String, Vertex> vertexMapping){
 
