e687693bc504004f7a077750d4eec1a1e7f9d8d1
Jack Huang <xhua451@UoA.auckland.ac.nz>
2017-08-22 16:47:56 +1200

    added more test cases

--- src/test/java/scheduler/io/TestInputParser.java
+++ src/test/java/scheduler/io/TestInputParser.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
+
 import io.InputParser;
 import io.InputParserException;
 
@@ -43,7 +44,7 @@ public class TestInputParser {
 		}
 		
 		@Test
-		public void testNonValidNoOption() {
+		public void testInvalidProcessorsNoOption() {
 			String[] input = new String[] {"input.dot", "0"};
 			InputParser parser;
 			try {
@@ -55,33 +56,6 @@ public class TestInputParser {
 			
 		}
 		
-		@Test
-		public void testVisualise() {
-			String[] input = new String[] {"input.dot", "1", "-v"};
-			InputParser parser = null;
-			try {
-			parser = new InputParser(input);
-			parser.parse();
-			
-			} catch (InputParserException e) {
-				fail();
-			}
-			assertTrue(parser.isVisualise());
-		}
-		
-		@Test
-		public void testValidProcessors() {
-			String[] input = new String[] {"input.dot", "1", "-v", "-p", "1"};
-			InputParser parser = null;
-			try {
-			parser = new InputParser(input);
-			parser.parse();
-			
-			} catch (InputParserException e) {
-				fail();
-			}
-			assertEquals(1, parser.toString());
-		}
 		
 		@Test
 		public void testAllOptionsValid() {
@@ -99,10 +73,57 @@ public class TestInputParser {
 			assertEquals(1, parser.getProcessors());
 			assertTrue(parser.isVisualise());
 			assertEquals(1, parser.getCores());
-			
-			
+			assertEquals("Yuan Wei.dot", parser.getOutputFileName());
+		}
+		
+		@Test
+		public void testInvalidCores() {
+			String[] input = new String[] {"input.dot", "1", "-p", "0"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
 			
 		}
+		
+		@Test
+		public void testInvalidOutputName() {
+			String[] input = new String[] {"input.dot", "3", "-o", "asdf"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+			
+		}
+		
+		@Test
+		public void testInvalidCaptitalOption() {
+			String[] input = new String[] {"input.dot", "3", "-O", "asdf.dot"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+			
+		}
+		
+		@Test
+		public void testInvalidInputFile() {
+			String[] input = new String[] {"input", "3"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+		}
+		
+		
 		@After
 		public void deinitialise() {
 			_file.delete();
