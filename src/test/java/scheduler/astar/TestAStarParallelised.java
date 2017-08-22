e453a556d015faed803be54dce76178a52919573
Unknown <h.chuk@auckland.ac.nz>
2017-08-22 21:55:12 +1200

    Additional Javadoc for AStar tests

--- src/test/java/scheduler/astar/TestAStarParallelised.java
+++ src/test/java/scheduler/astar/TestAStarParallelised.java
@@ -9,11 +9,17 @@ import org.junit.Test;
 import io.DataReader;
 import scheduler.graphstructures.DefaultDirectedWeightedGraph;
 
+/**
+ * Tests to make sure that the AStarParallelised class functions as expected 
+ */
 public class TestAStarParallelised {
 
 	//Data structures to use
 	DefaultDirectedWeightedGraph _graph = new DefaultDirectedWeightedGraph();
-	
+
+	/**
+	 * Test to check initialising the AStarParallelised class
+	 */
 	@Test
 	public void testConstructor(){
 		AStar alg = new AStarParallelised(_graph, 2, 3, null,null);
@@ -21,10 +27,13 @@ public class TestAStarParallelised {
 		assertEquals(_graph.edgeSet().size(),0);
 	}
 	
+	/**
+	 * TODO: Test for getting the topological sort result for a fork-join graph
+	 */
 	public void testOne(){
 		DataReader dataReader = new DataReader(new File("test-examples/test1.dot"));
 		while(dataReader.hasMoreGraphs()) {
-		dataReader.readNextGraph();
+			dataReader.readNextGraph();
 		}
 	}
 }
