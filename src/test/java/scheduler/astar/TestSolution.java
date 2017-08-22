9347a9b1ba47cfa5ccf699d8395695f919566a71
Unknown <h.chuk@auckland.ac.nz>
2017-08-22 15:18:16 +1200

    Added test case for graph 1 for AStar

--- src/test/java/scheduler/astar/TestSolution.java
+++ src/test/java/scheduler/astar/TestSolution.java
@@ -5,7 +5,11 @@ import scheduler.graphstructures.Vertex;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
+
+import io.DataReader;
 
 public class TestSolution {
 		
@@ -20,5 +24,16 @@ public class TestSolution {
 			AStar alg = new AStar(_graph, 1, null);
 			Solution sol = alg.execute();
 			assertEquals(10, sol.getLastFinishTime());
+		}
+		
+		@Test
+		public void testOne(){
+			DataReader dataReader = new DataReader(new File("test-examples/test1.dot"));
+			while(dataReader.hasMoreGraphs()) {
+			dataReader.readNextGraph();
+			AStar alg = new AStar(dataReader.getGraph(), 1, null);
+			Solution sol = alg.execute();
+			assertEquals(640, sol.getLastFinishTime());
+			}
 		}
 }
