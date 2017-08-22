98e2a6ff35d45e0e80666cf5e4c3db99d7c9cee0
Jack Huang <xhua451@UoA.auckland.ac.nz>
2017-08-22 17:14:15 +1200

    Finsihed TestInputParser

--- src/test/java/scheduler/io/TestInputParser.java
+++ src/test/java/scheduler/io/TestInputParser.java
@@ -59,7 +59,7 @@ public class TestInputParser {
 		
 		@Test
 		public void testAllOptionsValid() {
-			String[] input = new String[] {"input.dot", "1", "-v", "-p", "1", "-o", "Yuan Wei.dot"};
+			String[] input = new String[] {"input.dot", "1", "-v", "-p", "2", "-o", "Yuan Wei.dot"};
 			
 			InputParser parser = null;
 			try {
@@ -72,8 +72,42 @@ public class TestInputParser {
 			
 			assertEquals(1, parser.getProcessors());
 			assertTrue(parser.isVisualise());
+			assertTrue(parser.isParallelise());
+			assertEquals(2, parser.getCores());
+			assertEquals("Yuan Wei.dot", parser.getOutputFileName());
+		}
+		
+		@Test
+		public void testAllOptionsValidOtherOrder() {
+			String[] input = new String[] {"input.dot", "1", "-o", "Yuan Wei.dot", "-p", "1", "-v" };
+			
+			InputParser parser = null;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			
+			} catch (InputParserException e) {
+				fail();
+			}
+			
+			assertEquals(1, parser.getProcessors());
+			assertTrue(parser.isVisualise());
+			assertTrue(parser.isParallelise());
 			assertEquals(1, parser.getCores());
 			assertEquals("Yuan Wei.dot", parser.getOutputFileName());
+		}
+		
+		@Test
+		public void testFileAndProcessesLast() {
+			String[] input = new String[] {"-o", "Yuan Wei.dot", "-p", "1", "-v", "input.dot", "1" };
+			
+			InputParser parser = null;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			
+			} catch (InputParserException e) {
+			}
 		}
 		
 		@Test
@@ -115,6 +149,61 @@ public class TestInputParser {
 		@Test
 		public void testInvalidInputFile() {
 			String[] input = new String[] {"input", "3"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+		}
+		
+		@Test
+		public void testWrongOrderNoOptions() {
+			String[] input = new String[] {"3", "input.dot", };
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+		}
+		
+		@Test
+		public void testWrongOrderAllOptions() {
+			String[] input = new String[] {"input.dot", "1", "-v", "-p", "1", "Yuan Wei.dot", "-o"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+		}
+		
+		@Test
+		public void testOptionNoArgument() {
+			String[] input = new String[] {"input.dot", "1", "-p"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+		}
+		
+		@Test
+		public void testMultipleParametersAfterOption() {
+			String[] input = new String[] {"input.dot", "1", "-p", "1", "2"};
+			InputParser parser;
+			try {
+			parser = new InputParser(input);
+			parser.parse();
+			} catch (InputParserException e) {
+			}
+		}
+		
+		@Test
+		public void testNoDash() {
+			String[] input = new String[] {"input.dot", "1", "p", "1"};
 			InputParser parser;
 			try {
 			parser = new InputParser(input);
