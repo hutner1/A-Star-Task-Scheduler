d6a34009f0591b50015704a5b1e63a4408af7c90
Jack Huang <xhua451@UoA.auckland.ac.nz>
2017-08-24 12:13:01 +1200

    fixed legend

--- src/main/java/visualization/gui/Legend.java
+++ src/main/java/visualization/gui/Legend.java
@@ -105,7 +105,7 @@ public class Legend extends JPanel {
 		JTextField text = new JTextField();
 		text.setEditable(false);
 		text.setFont(new Font("SansSerif", Font.BOLD, 10));
-		text.setText("Processor "+currentProcessor);
+		text.setText("Processor "+(currentProcessor+1));
 		text.setBounds(25, 0, 90, 20);
 		text.setBorder(BorderFactory.createEmptyBorder());
 		panel.add(text);
